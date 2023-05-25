eb43392869ed8399a64d1fab5d5b0d666d575b42
willamjonathan <williamjonathan1289@gmail.com>
2023-05-25 18:59:21 +0700

    "drafted"

--- sql_app/main.py
+++ sql_app/main.py
@@ -89,6 +89,14 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(email: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=email)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
